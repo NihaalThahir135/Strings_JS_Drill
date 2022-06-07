0ee386e0e1c2508419c3bd75ce603ba81e522deb
NihaalThahir135 <106650531+NihaalThahir135@users.noreply.github.com>
2022-06-07 17:17:56 +0530

    Add files via upload

---
+++ testString3.js
@@ -0,0 +1,6 @@
+// ==== String Problem #3 ====
+// Given a string in the format of "10/1/2021", print the month in which the date is present in.
+let inputDate="10/1/2021";
+let {string3}= require('../string3');
+let result=string3(inputDate);
+console.log(result);
