2148b8822a5be065ef096f960713abb0e1bc3d18
NihaalThahir135 <106650531+NihaalThahir135@users.noreply.github.com>
2022-06-07 17:18:15 +0530

    Add files via upload

---
+++ testString4.js
@@ -0,0 +1,7 @@
+// ==== String Problem #4 ====
+// Given an object in the following format, return the full name in title case.
+// {"first_name": "JoHN", "last_name": "SMith"}
+// {"first_name": "JoHN", "middle_name": "doe", "last_name": "SMith"}
+let {string4}=require('../string4');
+let inputName = {"first_name": "JoHN", "last_name": "SMith"};
+console.log(string4(inputName));
